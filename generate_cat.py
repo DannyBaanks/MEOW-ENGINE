ec50b5d4e6b2fd5f3a569aa025246e1d3fb9b72a
DannyBaanks <danielbaanksyy@gmail.com>
2026-08-16 18:41:50 -0600

    🐱 Fix CI: script Python separado para gatito (evita errores YAML multilínea)

---
+++ generate_cat.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Genera un gatito ASCII aleatorio y lo guarda en cat.txt"""
+import random
+import sys
+
+CATS = [
+    r"""      |\      _,,,---,,_
+ZZZzz /,`.-'`'    -.  ;-;;,_
+     |,4-  ) )-,_. ,\ (  '-' 
+    '---''(_/--'  `-\_)""",
+    r"""      |\      _,,,---,,_
+ZZZzz /,`.-'`'    -.  ;-;;,_
+     |,4-  ) )-,_. ,\ (  '-' 
+    '---''(_/--'  \_-')""",
+    r"""      |\      _,,,---,,_
+ZZZzz /,`.-'`'    -.  ;-;;,_
+     |,4-  ) )-,_. ,\ (  '-' 
+    '---''(_/--'  \_\')""",
+    r"""      |
+   / \__
+  (    @\___
+  /         O
+ /   (_____/
+/_____/   U""",
+    r"""      |
+    \   /
+     |  |
+    /   \
+   |     |
+   |  .  |
+    \___/
+    ___|___"""
+]
+
+def main():
+    cat = random.choice(CATS)
+    with open("cat.txt", "w", encoding="utf-8") as f:
+        f.write(cat)
+    print(cat)
+
+if __name__ == "__main__":
+    main()
